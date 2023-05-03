54330da98d1769bc595f40c1b20a6dfe9b78c1d4
AKCHAT0211 <94164853+AKCHAT0211@users.noreply.github.com>
2023-05-04 02:42:42 +0530

    here is the code for triangle pattern

--- triangle.java
+++ triangle.java
@@ -1,65 +1,70 @@
-
 import java.util.Scanner;
 class triangle {
     public static void main(String[] args) {
         int i,j=0;
         Scanner sc = new Scanner (System.in);
-       System.out.print("Enter ur name:");
-        String n= sc.nextLine();
-        System.out.print("ur age is :");
-       int age=sc.nextInt();
-         System.out.println("choose 1 for ascendiong order triangle  2 for decending triangle 3 for upside-down triangle 4 for downside-up triangle");
+        System.out.println(":-) Welcome To Pattern World :-)");
+        System.out.println(" ");
+        System.out.println("Choose 1 :- for ascendiong order triangle  2 :- for decending triangle");
+        System.out.println(" 3 :- for upside-down triangle 4 :- for downside-up triangle");
+        System.out.print("Enter your choice:");
         int ch=sc.nextInt();
-        switch(ch)
-        {
-            case 1:
-                System.out.print("Enter the no of lines:");
-               int l=sc.nextInt();
-              for(i=0;i<=l;i++){
-                    for(j=0;j<=i;j++){
-                        System.out.print("*");
-                        }
-                        System.out.println(" ");
-        } break;
+        switch(ch) {
+        case 1:
+            System.out.print("Enter the no of lines:");
+            int l=sc.nextInt();
+            for(i=1; i<=l; i++) {
+                for(j=1; j<=i; j++) {
+                    System.out.print("*");
+                }
+                System.out.println(" ");
+            }
+            break;
         case 2:
-             System.out.print("Enter the no of lines:");
-               int r=sc.nextInt();
-              for(i=r;i>=0;i--){
-                    for(j=0;j<=i;j++){
-                        System.out.print("*");
-                        }
-                        System.out.println(" ");}
-                        break;
-            case 3:
-             System.out.print("Enter the no of lines:");
-               int k=sc.nextInt();
-              for(i=0;i<=k;i++){
-                    for(j=0;j<=i;j++){
-                        System.out.print("*");
-                        }
-                        System.out.println(" ");}
-                          for(i=k;i>=0;i--){
-                    for(j=0;j<=i;j++){
-                        System.out.print("*");
-                        }
-                        System.out.println(" "); }
-                        break;
-                        case 4:
-                             System.out.print("Enter the no of lines:");
-               int s=sc.nextInt();
-              for(i=0;i<=s;i++){
-                    for(j=0;j<=i;j++){
-                        System.out.print("*");
-                        }
-                        System.out.println(" ");}
-                              for(i=s;i>=0;i--){
-                    for(j=0;j<=i;j++){
-                        System.out.print("*");
-                        }
-                        System.out.println(" "); 
-                              }break;
-    default:System.out.print("wrong output");
-}
+            System.out.print("Enter the no of lines:");
+            int r=sc.nextInt();
+            for(i=r; i>=1; i--) {
+                for(j=1; j<=i; j++) {
+                    System.out.print("*");
+                }
+                System.out.println(" ");
+            }
+            break;
+        case 3:
+            System.out.print("Enter the no of lines:");
+            int k=sc.nextInt();
+            for(i=1; i<=k; i++) {
+                for(j=1; j<=i; j++) {
+                    System.out.print("*");
+                }
+                System.out.println(" ");
+            }
+            for(i=k; i>=1; i--) {
+                for(j=1; j<=i; j++) {
+                    System.out.print("*");
+                }
+                System.out.println(" ");
+            }
+            break;
+        case 4:
+            System.out.print("Enter the no of lines:");
+            int s=sc.nextInt();
+            for(i=s; i>=1; i--) {
+                for(j=1; j<=i; j++) {
+                    System.out.print("*");
+                }
+                System.out.println(" ");
+            }
+            for(i=2; i<=s; i++) {
+                for(j=1; j<=i; j++) {
+                    System.out.print("*");
+                }
+                System.out.println(" ");
+            }
+            break;
+        default:
+            System.out.print("wrong output");
+        }
     }
 
 }
